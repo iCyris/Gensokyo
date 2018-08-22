b6408d8cb83e1f09c3c3f2cfdb20ef57b2ba6e87
iCyris <cyris1015@gmail.com>
2018-08-22 20:30:42 +0800

    update js

---
+++ assets/js/page.js
@@ -0,0 +1,19 @@
+$(function(){
+	getMsg();
+});
+
+function getMsg(){
+	var slogan = [
+		"一期一会，为之尽兴，珍惜而待。",
+        "信じるそれだけで，越えられないものはない。",
+        "yuziyuzi, yuzazi~ ♫"
+	];
+	var r = random(0,slogan.length-1);
+	$("#slogan").html(slogan[r])
+}
+function random(Min,Max){
+      var Range = Max - Min;
+      var Rand = Math.random();
+      var num = Min + Math.round(Rand * Range);
+      return num;
+}
