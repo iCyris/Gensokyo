fa06853f5702313d9f0dad18293281da4178162d
iCyris <cyris1015@gmail.com>
2018-08-29 08:42:44 +0800

    add some words

--- assets/js/page.js
+++ assets/js/page.js
@@ -6,7 +6,9 @@ function getMsg(){
 	var slogan = [
 	    "一期一会，为之尽兴，珍惜而待。",
           "信じるそれだけで，越えられないものはない。",
-          "yuziyuzi, yuzazi~ ♫"
+          "嗯哼哼，哒哒哒～ ♫",
+          "想要成为她的一片海，用水般的柔软和细腻去轻抚和守护。",
+          "这里的生活是玫瑰色的！"
 	];
 	var r = random(0,slogan.length-1);
 	$("#slogan").html(slogan[r])
