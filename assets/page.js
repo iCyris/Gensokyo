e9f0d453c2fb2f4b92a214e3c989f3ae2a831c9a
iCyris <cyris1015@gmail.com>
2018-09-16 00:35:30 +0800

    happy birthday! Version-5.0

---
+++ assets/page.js
@@ -0,0 +1,35 @@
+function ShowHide(obj)
+{
+    if(obj.style.display === "block")
+    {
+        obj.style.display = 'none';
+    }
+    else
+    {
+        obj.style.display = 'block';
+    }
+}
+function quq(obj)
+{
+    var textNode = obj.childNodes[0];
+    if(textNode.nodeValue === "Welcome")
+    {
+        textNode.nodeValue = 'Close';
+    }
+    else
+    {
+        textNode.nodeValue = 'Welcome';
+    }
+}
+window.onload=function()
+{
+    var btn = document.getElementById("button");
+    var about = document.getElementById("about");
+    var hi = document.getElementById("hi");
+    btn.onclick = function()
+    {
+        quq(hi);
+        ShowHide(about);
+        return false;
+    };
+};
